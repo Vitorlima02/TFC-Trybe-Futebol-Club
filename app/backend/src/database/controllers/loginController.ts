46df2ee40d5aedf63a0d5671b0505b2b44a14105
Vitor Lima <vitorliman@gmail.com>
2022-03-24 22:08:57 -0300

    impl validação do token

--- app/backend/src/database/controllers/loginController.ts
+++ app/backend/src/database/controllers/loginController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
 import LoginService from '../services/loginService';
 import StatusCode from '../middlewares/statusCode';
+import User from '../models/User';
+
+const secret = fs.readFileSync('jwt.evaluation.key', 'utf-8');
 
 export default class LoginController {
   static async login(req: Request, res: Response) {
@@ -13,5 +18,21 @@ export default class LoginController {
     }
 
     return res.status(StatusCode.OK).json(response);
+  }
+
+  static async validateLogin(req: Request, res: Response) {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(StatusCode.INVALIDREQ).json({ message: 'Token not found' });
+
+    const decoded = jwt.verify(token, secret) as jwt.JwtPayload;
+
+    req.body = decoded.info;
+
+    const { email } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (user) return res.status(StatusCode.OK).json(user.role);
   }
 }

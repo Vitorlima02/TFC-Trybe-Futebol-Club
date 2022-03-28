53ef2354922857e9bdade856dce642d34371facf
Vitor Lima <vitorliman@gmail.com>
2022-03-28 15:49:49 -0300

    impl add uma nova Match

--- app/backend/src/database/services/matchsService.ts
+++ app/backend/src/database/services/matchsService.ts
@@ -1,5 +1,6 @@
 import Clubs from '../models/Club';
 import Match from '../models/Matchs';
+import IMatch from '../interfaces/iMatch';
 
 export default class MatchService {
   static async getAllMatchs() {
@@ -11,5 +12,30 @@ export default class MatchService {
     if (getAllMatchs.length === 0) return { message: 'Matchs not found' };
 
     return getAllMatchs;
+  }
+
+  static async addMatchs(data: IMatch) {
+    const [homeTeamName, awayTeamName] = await Promise.all([
+      Clubs.findOne({ where: { id: data.homeTeam } }),
+      Clubs.findOne({ where: { id: data.awayTeam } }),
+    ]);
+
+    if (!homeTeamName || !awayTeamName) {
+      return { message: 'There is no team with such id!' };
+    }
+
+    const addMatch = await Match.create(data);
+
+    if (!addMatch) return { message: 'Match not created' };
+
+    const result = {
+      id: addMatch.id,
+      homeTeam: addMatch.homeTeam,
+      homeTeamGoals: addMatch.homeTeamGoals,
+      awayTeam: addMatch.awayTeam,
+      awayTeamGoals: addMatch.awayTeamGoals,
+      inProgress: addMatch.inProgress,
+    };
+    return result;
   }
 }
